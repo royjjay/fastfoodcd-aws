6c7016dbf7c42738b5646bc9552aeeb5fb07215d
royjjay <royjay@me.com>
2020-08-19 19:33:31 -0400

    commit test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
+/* Is Git still working? */
+
 function App() {
   return (
     <div className="App">
